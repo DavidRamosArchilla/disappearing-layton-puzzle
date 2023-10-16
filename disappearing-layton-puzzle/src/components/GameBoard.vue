60c84279873d474b5353dc1598c788ac9e76cc3c
Davidram1802 <56789396+Davidram1802@users.noreply.github.com>
2023-10-16 16:00:45 +0200

    board reshaped to the correct one

--- disappearing-layton-puzzle/src/components/GameBoard.vue
+++ disappearing-layton-puzzle/src/components/GameBoard.vue
@@ -5,17 +5,22 @@
         v-for="(piece, colIndex) in row"
         :key="colIndex"
         class="square"
-        :class="{ dark: (rowIndex + colIndex) % 2 === 1 }"
+        :class="{ non_empty:piece != null }"
+        
         @dragover.prevent
         @drop="handleDrop(rowIndex, colIndex)"
       >
         <div
-          v-if="piece"
+          v-if="piece === 1"
           class="piece"
           draggable="true"
-          @dragstart="handleDragStart(rowIndex, colIndex)"
+          @dragstart="handleDragStart($event, rowIndex, colIndex)"
         >
-          {{ piece }}
+          <!-- {{ piece }} -->
+          <img
+            :src="getPieceImage(piece)"
+            alt="Piece"
+          />
         </div>
       </div>
     </div>
@@ -26,31 +31,48 @@
 export default {
   data() {
     return {
-      board: [
-        ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-        ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-        Array(8).fill(null),
-        Array(8).fill(null),
-        Array(8).fill(null),
-        Array(8).fill(null),
-        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
-      ],
+      board: this.getInitalBoard(),
       selectedPiece: null,
       selectedPiecePosition: null
     };
   },
   methods: {
-    handleDragStart(rowIndex, colIndex) {
+    getInitalBoard(){
+      const sample = require('@/puzzles/puzzle_1.json'); 
+      var puzzle = sample.board;
+      puzzle = puzzle.map(arr => arr.map(item => item === -1 ? null : item))
+      // return [
+      //   ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+      //   ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+      //   Array(8).fill(null),
+      //   Array(8).fill(null),
+      //   Array(8).fill(null),
+      //   Array(8).fill(null),
+      //   ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+      //   ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+      // ];
+      return puzzle;
+    },
+    getPieceImage(piece){
+      if(piece === 1)
+        return require('@/assets/logo.png');
+      // else
+      //   return require('@/assets/back.png');
+      
+    },
+    handleDragStart(event, rowIndex, colIndex) {
       if (this.board[rowIndex][colIndex]) {
         this.selectedPiece = this.board[rowIndex][colIndex];
         this.selectedPiecePosition = { rowIndex, colIndex };
+        event.dataTransfer.effectAllowed = 'move'; // Specify the effect
+        event.dataTransfer.setData('text/plain', ''); // Necessary for Firefox
+        // event.preventDefault(); // Prevent the default behavior
       }
     },
     handleDrop(rowIndex, colIndex) {
-      if (this.selectedPiece) {
+      if (this.selectedPiece && (this.board[rowIndex][colIndex] != null)) {
         this.board[rowIndex].splice(colIndex, 1, this.selectedPiece);
-        this.board[this.selectedPiecePosition.rowIndex].splice(this.selectedPiecePosition.colIndex, 1, null);
+        this.board[this.selectedPiecePosition.rowIndex].splice(this.selectedPiecePosition.colIndex, 1, 0);
         this.selectedPiece = null;
         this.selectedPiecePosition = null;
       }
@@ -75,11 +97,24 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 1px solid #000;
+  /* border: 1px solid #000;
+   background-image: url('@/assets/back.png');
+  background-size: cover; */
 }
 
-.dark {
+.square img {
+  width: 100%; /* Ensure the image fills the square */
+  height: 100%; /* Ensure the image fills the square */
+  object-fit: contain; /* Maintain the aspect ratio while fitting */
+}
+
+/*.dark {
   background-color: #b58863;
+}*/
+
+.non_empty{
+  background-image: url('@/assets/back.png');
+  background-size: cover; 
 }
 
 .piece {
